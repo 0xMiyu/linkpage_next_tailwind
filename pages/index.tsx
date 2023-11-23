61e05028269f8c5d46ea7e69d4b3a4f6349e60b6
0xMiyu <ong.martin24@gmail.com>
2023-11-24 01:44:08 +0800

    feat: added cal and hover transition

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import { BsGithub, BsLinkedin, BsTelegram, BsTwitter, BsWhatsapp } from "react-icons/bs";
+import { BsGithub, BsLinkedin, BsTelegram, BsTwitter, BsWhatsapp, BsCalendar2 } from "react-icons/bs";
 import {AiOutlineMail} from "react-icons/ai"
 
 const inter = Inter({ subsets: ["latin"] });
 
 const GITHUB_LINK = "https://github.com/0xmiyu"
 const LINKEDIN_LINK = "https://www.linkedin.com/in/martinongyj/" 
+const CAL_LINK = "https://cal.com/martin-zephyr"
 const TWITTER_LINK = "https://twitter.com/0xmiyu"
 const TELEGRAM_LINK = "https://t.me/miyu_sol"
 const WHATSAPP_LINK = "https://wa.me/6597114549"
@@ -15,7 +16,7 @@ const EMAIL_LINK = "mailto:martin@zephyrlabs.ai"
 
 export default function Home() {
     return (
-        <div className="bg-gray-900 text-2xl h-screen">
+        <div className="bg-gray-900 h-screen text-2xl">
             <Head>
                 <title>Miyu&apos;s Links</title>
                 <meta
@@ -40,7 +41,7 @@ export default function Home() {
 
                 <div className="flex items-center justify-center">
                     <ul className="font-mono text-xl table border-5 border-white w-7/12 text-center">
-                        <li className="p-3 my-2 mx-4 bg-black rounded-lg hover:bg-gray-300 hover:text-black">
+                        <li className="p-3 my-2 mx-4 bg-black rounded-lg hover:bg-gray-300 hover:text-black transition duration-265 ease-in-outa">
                             <div className="flex items-center">
                                 <BsGithub className="ml-auto" />
 
@@ -54,7 +55,7 @@ export default function Home() {
                                 </a>
                             </div>
                         </li>
-                        <li className="p-3 mb-2 mx-4 bg-black rounded-lg hover:bg-gray-300 hover:text-black">
+                        <li className="p-3 mb-2 mx-4 bg-black rounded-lg hover:bg-gray-300 hover:text-black transition duration-265 ease-in-outa">
                             <div className="flex items-center">
                                 <BsLinkedin className="ml-auto" />
                                 <a
@@ -67,7 +68,19 @@ export default function Home() {
                                 </a>
                             </div>
                         </li>
-                        
+                        <li className="p-3 mb-2 mx-4 bg-black rounded-lg hover:bg-gray-300 hover:text-black transition duration-265 ease-in-outa">
+                            <div className="flex items-center">
+                                <BsCalendar2 className="ml-auto" />
+                                <a
+                                    href={CAL_LINK}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="flex-1 text-center"
+                                >
+                                    Book a Meeting!
+                                </a>
+                            </div>
+                        </li>
                         {/* <li className="p-3 mb-4 mx-4 bg-black rounded-lg hover:bg-gray-300 hover:text-black">
                             <div className="flex items-center">
                                 <BsTwitter className="ml-auto" />
@@ -81,7 +94,7 @@ export default function Home() {
                                 </a>
                             </div>
                         </li> */}
-                        <li className="p-3 mb-4 bg-gray-800 rounded-3xl hover:bg-gray-300 hover:text-black">
+                        <li className="p-3 mb-4 bg-gray-800 rounded-3xl hover:bg-gray-300 hover:text-black transition duration-265 ease-in-outa">
                             <div className="flex items-center">
                                 <BsTelegram className="ml-4" />
                                 <a
@@ -94,7 +107,7 @@ export default function Home() {
                                 </a>
                             </div>
                         </li>
-                        <li className="p-3 mb-4 bg-gray-800 rounded-3xl hover:bg-gray-300 hover:text-black">
+                        <li className="p-3 mb-4 bg-gray-800 rounded-3xl hover:bg-gray-300 hover:text-black transition duration-265 ease-in-outa">
                             <div className="flex items-center">
                                 <BsWhatsapp className="ml-4" />
                                 <a
@@ -107,7 +120,7 @@ export default function Home() {
                                 </a>
                             </div>
                         </li>
-                        <li className="p-3 mb-2 bg-gray-800 rounded-3xl hover:bg-gray-300 hover:text-black">
+                        <li className="p-3 mb-2 bg-gray-800 rounded-3xl hover:bg-gray-300 hover:text-black transition duration-265 ease-in-outa">
                             <div className="flex items-center">
                                 <AiOutlineMail className="ml-4" />
                                 <a
